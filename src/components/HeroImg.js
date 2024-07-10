f7137994d1750a469b6d6fae72c94a48da75f1fe
Deepanshu Sharma <sharmadeepanshu1005@gmail.com>
2024-07-10 14:01:43 +0530

    Heading changed

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -12,8 +12,8 @@ const HeroImg = () => {
             <img className="intro-img" src={IntroImg} alt="IntroImg" />
         </div>
         <div className="content">
-            <p>HI, I'M A FREELANCER.</p>
-            <h1>React Developer.</h1>
+            <p style={{fontSize: '1.5rem'}}>Greetings from Deepanshu Sharma !!</p>
+            <h1>React <span style={{color: '#f8d90e'}}> Developer.</span></h1>
             <div>
                 <Link to="/project" className="btn">PROJECTS</Link>
                 <Link to="/contact" className="btn btn-light">CONTACT</Link>
