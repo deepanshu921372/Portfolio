d3cd01d4123c8c202691da7e06c3a2c01852aa33
Deepanshu Sharma <sharmadeepanshu1005@gmail.com>
2024-07-10 22:17:04 +0530

    added projects

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -2,6 +2,10 @@ import Netflix from "../assets/Netflix.png"
 import Twitter from "../assets/Twitter.png"
 import ToDo from "../assets/ToDo.jpeg"
 import Pass from "../assets/Pass.jpeg"
+import Employee from "../assets/Employee.png"
+import TextUtils from "../assets/TextUtils.png"
+import Nykaa from "../assets/Nykaa.png"
+import Restaurant from "../assets/Restaurant.png"
 
 
 const ProjectCardData = [
@@ -32,6 +36,35 @@ const ProjectCardData = [
         text : "Developed a robust Password Manager web app using React.js and Node.js, ensuring secure storage and management of user credentials. Implemented advanced encryption techniques for enhanced security and seamless user experience.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720366637329/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
         source : "https://github.com/deepanshu921310/PasswordManager"
+    },
+    {
+        imgsrc: Employee,
+        title : "Employee Management Web App",
+        text : "Developed an Employee Management Web App using Material-UI, React.js, Node.js, Postman, MongoDB, and Amazon S3. The app streamlines employee registration by capturing essential data and generating unique ID cards. This project highlights my skills in creating efficient and user-friendly web solutions.",
+        view : "https://admin.999logistics.in/",
+        source : "https://github.com/deepanshu921310/999-manager-app-id-card"
+    },
+    {
+        imgsrc: TextUtils,
+        title : "TextUtils-Text Transformator",
+        text : "TextUtils is a React app built with React.js and Material-UI, designed for efficient text manipulation. It offers features like word counting, character counting, removing extra spaces, and converting text to uppercase and lowercase, showcasing proficiency in creating functional and user-friendly web applications.",
+        view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720625521851/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
+        source : "https://github.com/deepanshu921310/TextUtils-React"
+    },
+    {
+        imgsrc: Nykaa,
+        title : "TextUtils-Text Transformator",
+        text : "Created a Nykaa clone using HTML, CSS, and JavaScript, featuring product listings, and checkout processes. Demonstrates proficiency in front-end web development and building responsive e-commerce interfaces.",
+        view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720626238317/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
+        source : "https://github.com/deepanshu921310/Nykka-Clone"
+    }
+    ,
+    {
+        imgsrc: Restaurant,
+        title : "Restaurant Website",
+        text : "Restaurant's essence with a sleek website powered by React.js and Material-UI. Combining elegant design with seamless navigation, explore our menu and services effortlessly, reflecting our commitment to exceptional dining experiences.",
+        view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720626914799/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
+        source : "https://github.com/deepanshu921310/restaurant-website-MUI"
     }
 ];
 
