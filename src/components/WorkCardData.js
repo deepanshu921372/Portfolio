3e9bb945d7d69b6bf967bf1f37d25fafe1c51840
Deepanshu Sharma <sharmadeepanshu1005@gmail.com>
2024-07-11 13:27:55 +0530

    Github id changed

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -14,49 +14,49 @@ const ProjectCardData = [
         title : "Netflix Clone",
         text : "Developed a Netflix clone using HTML and CSS, showcasing a sleek, responsive design that mirrors the original platform. Demonstrated strong front-end skills and attention to detail in recreating the user interface.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720366019980/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
-        source : "https://github.com/deepanshu921310/Netflix"
+        source : "https://github.com/deepanshu921372/Netflix"
     },
     {
         imgsrc: Twitter,
         title : "Twitter Clone",
         text : "Built a Twitter clone with HTML and CSS, achieving a polished and responsive design that replicates the original. Showcased my front-end expertise and eye for detail in recreating the user experience.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720366245453/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
-        source : "https://github.com/deepanshu921310/Twitter"
+        source : "https://github.com/deepanshu921372/Twitter"
     },
     {
         imgsrc: ToDo,
         title : "ToDo Web App",
         text : "Developed a dynamic ToDo web application utilizing React.js and Node.js, ensuring efficient task management and seamless user experience. Enhanced functionality and responsiveness to create a modern, user-friendly interface.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720366412541/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
-        source : "https://github.com/deepanshu921310/ToDoList"
+        source : "https://github.com/deepanshu921372/ToDoList"
     },
     {
         imgsrc: Pass,
         title : "Password Manger Web App",
         text : "Developed a robust Password Manager web app using React.js and Node.js, ensuring secure storage and management of user credentials. Implemented advanced encryption techniques for enhanced security and seamless user experience.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720366637329/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
-        source : "https://github.com/deepanshu921310/PasswordManager"
+        source : "https://github.com/deepanshu921372/PasswordManager"
     },
     {
         imgsrc: Employee,
         title : "Employee Management Web App",
         text : "Developed an Employee Management Web App using Material-UI, React.js, Node.js, Postman, MongoDB, and Amazon S3. The app streamlines employee registration by capturing essential data and generating unique ID cards. This project highlights my skills in creating efficient and user-friendly web solutions.",
         view : "https://admin.999logistics.in/",
-        source : "https://github.com/deepanshu921310/999-manager-app-id-card"
+        source : "https://github.com/deepanshu921372/999-manager-app-id-card"
     },
     {
         imgsrc: TextUtils,
         title : "TextUtils-Text Transformator",
         text : "TextUtils is a React app built with React.js and Material-UI, designed for efficient text manipulation. It offers features like word counting, character counting, removing extra spaces, and converting text to uppercase and lowercase, showcasing proficiency in creating functional and user-friendly web applications.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720625521851/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
-        source : "https://github.com/deepanshu921310/TextUtils-React"
+        source : "https://github.com/deepanshu921372/TextUtils-React"
     },
     {
         imgsrc: Nykaa,
         title : "Nykaa Clone",
         text : "Created a Nykaa clone using HTML, CSS, and JavaScript, featuring product listings, and checkout processes. Demonstrates proficiency in front-end web development and building responsive e-commerce interfaces.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720626238317/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
-        source : "https://github.com/deepanshu921310/Nykka-Clone"
+        source : "https://github.com/deepanshu921372/Nykka-Clone"
     }
     ,
     {
@@ -64,7 +64,7 @@ const ProjectCardData = [
         title : "Restaurant Website",
         text : "Restaurant's essence with a sleek website powered by React.js and Material-UI. Combining elegant design with seamless navigation, explore our menu and services effortlessly, reflecting our commitment to exceptional dining experiences.",
         view : "https://www.linkedin.com/in/deepanshusharma921372/details/projects/1720626914799/single-media-viewer/?profileId=ACoAAEI93RMBwYY_8nGeXy7SQkdhChIXJ1BOS5U",
-        source : "https://github.com/deepanshu921310/restaurant-website-MUI"
+        source : "https://github.com/deepanshu921372/restaurant-website-MUI"
     }
 ];
 
